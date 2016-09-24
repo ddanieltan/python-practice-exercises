f9e834d6dfd05e65f8043469582d9376412f826f
Daniel Tan <danieltan.singapore@gmail.com>
2016-09-24 20:28:06 +0800

    added google python exercises

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # python-practice-exercises
 A collection of Python exercises
 
-The directory _practicepython.org_ contain Python exercises taken from [www.practicepython.org]
+The directory __practicepython.org__ contain Python exercises taken from [www.practicepython.org](Practice Python)
---
+++ practicepython.org/1-character-input.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 24 19:43:53 2016
+
+@author: ddan
+"""
+
+#==============================================================================
+# Create a program that asks the user to enter their name and their age. 
+# Print out a message addressed to them that tells them the year that they will turn 100 years old.
+# 
+# Extras:
+# 
+# Add on to the previous program by asking the user for another number 
+# and printing out that many copies of the previous message. 
+# (Hint: order of operations exists in Python)
+# Print out that many copies of the previous message on separate lines. 
+# (Hint: the string "\n is the same as pressing the ENTER button)
+#==============================================================================
+
+import datetime
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+print(name + ", you will turn 100 years old in " + 100)
