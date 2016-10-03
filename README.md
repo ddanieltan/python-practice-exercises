c093bd21053f1d378fc34e331d8bc7c49e2b9be9
Daniel Tan <danieltan.singapore@gmail.com>
2016-10-03 23:43:20 +0800

    updated readme

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ A collection of Python practice exercises
 * The directory __practicepython.org__ contain Python exercises taken from [Practice Python](www.practicepython.org).
 * The directory __google-python-exercises__ contain Python exercises taken from [Google's Python Class](https://developers.google.com/edu/python/exercises/basic).
 * The directory __hard-way__ contain Python exercises taken from [Learn Python The Hard Way](https://learnpythonthehardway.org/book/).
+* The directory __nump-100__ contain Python exercises taken from [100 Numpy Exercises](https://github.com/rougier/numpy-100).
--- google-python-exercises/logpuzzle/day2_os.py
+++ google-python-exercises/logpuzzle/day2_os.py
@@ -6,16 +6,35 @@ Created on Fri Sep 30 21:09:47 2016
 """
 import sys
 import os
+import commands
 
 #dir(os)
 
 def list_directory(dir1):
-  filenames = os.listdir(dir1)
-  return filenames
+  cmd = 'ls -l ' + dir1
+  (status,output) = commands.getstatusoutput(cmd)
+  if status: #if status is non-zero
+    sys.stderr.write('there was an error' + output)
+    sys.exit(1) #exit the script
+  print output
+
+#  filenames = os.listdir(dir1)
+#  for filename in filenames:
+#    path = os.path.join(dir1,filename)
+#    print path
+#    print os.path.abspath(path)
+  
   
 def main():
-  print list_directory(sys.argv[1])
+  list_directory(sys.argv[1])
 
 if __name__ == '__main__':
   main()
-  
+
+# os.path.exists()
+
+# import shutil
+# shutil.copy(source,dest)
+
+# import commands
+# commands.getstatusoutput(cmd)
